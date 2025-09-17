352b760afd21cae0749838ee48705649ea3dae16
Abdullahi Alimi <96649267+abdvllJSX@users.noreply.github.com>
2025-09-17 13:31:54 +0100

    fixing rediurects

--- src/components/shared/landing/features.tsx
+++ src/components/shared/landing/features.tsx
@@ -1,25 +1,24 @@
-import { title } from "process";
 
 const Features = () => {
     
-    const features = [
-        {
-            icons: "",
-            title: "Enterprise Collaboration",
-            pargragh: "Manage up to 1000 wallets, get instant insights.",
-        },
-        {
-            icons: "",
-            title: "Periodic Onchain Statement",
-            pargragh: "Compare wallets 1v1, 2v2, or bulk vs bulk.",
-        },
-        {
-            icons: "",
-            title: "Multi-Chain Support",
-            pargragh: "Analyze thousands of wallets at once across multiple chains ",
-        },
+    // const features = [
+    //     {
+    //         icons: "",
+    //         title: "Enterprise Collaboration",
+    //         pargragh: "Manage up to 1000 wallets, get instant insights.",
+    //     },
+    //     {
+    //         icons: "",
+    //         title: "Periodic Onchain Statement",
+    //         pargragh: "Compare wallets 1v1, 2v2, or bulk vs bulk.",
+    //     },
+    //     {
+    //         icons: "",
+    //         title: "Multi-Chain Support",
+    //         pargragh: "Analyze thousands of wallets at once across multiple chains ",
+    //     },
 
-    ]
+    // ]
 
     return ( 
         <section className="">
