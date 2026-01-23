ca36837f3ab70573bc26858407ad9d784cc6e330
Abdullahi Alimi <96649267+abdvllJSX@users.noreply.github.com>
2026-01-23 17:51:28 +0100

    BREAKINH CHANGE: landing page redo

--- src/components/shared/landing/hero.tsx
+++ src/components/shared/landing/hero.tsx
@@ -1,9 +1,60 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
+interface Coins {
+    image: string;
+    classname: string;
+    name: string;
+}
+
 const Hero = () => {
+    const coins: Coins[] = [
+        {
+            image: "/common/btc.png",
+            classname: "top-[5rem] left-[6.8rem] rotate-[-20deg] size-[31rem]",
+            name: "BTC"
+        },
+        {
+            image: "/common/binance.png",
+            classname: "size-[17rem] top-[53rem] rotate-37 left-[10rem]",
+            name: "Binance"
+        },
+        {
+            image: "/common/lite.png",
+            classname: "size-[17rem] top-[66rem] left-[38rem] rotate-19",
+            name: "Litecoin"
+        },
+        {
+            image: "/common/stellar.png",
+            classname: "size-[15rem] top-[77rem]  left-[76rem] rotate-[-12deg]",
+            name: "Stellar"
+        },
+        {
+            image: "/common/eth.png",
+            classname: "size-[19rem] top-[54rem] left-[97rem] rotate-[-22deg]",
+            name: "Ethereum"
+        },
+        {
+            image: "/common/cardano.png",
+            classname: "size-[22rem] top-[18.5rem] left-[104rem] rotate-[-27deg]",
+            name: "Cardano"
+        },
+
+    ]
     return (
         <section className="flex flex-col">
+            {
+                coins.map((coin) => {
+                    return (
+                        <img
+                            src={coin.image}
+                            alt={coin.name}
+                            className={`${coin.classname} absolute`}
+                            key={coin.name}
+                        />
+                    )
+                })
+            }
             <h1 className="sm:text-[5.5rem] text-[3rem] sm:mt-[17rem] mt-[6rem] text-[#FFFFFF] font-[400] mx-auto capitalize text-center sm:w-[50rem] w-[27rem]">
                 Analyze wallets across EVM, Bitcoin & Solana Chains.
             </h1>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,5 @@
+import MaxContainer from "@/components/shared/common/maxcontainer";
+
 import Nav from "@/components/shared/landing/navbar";
 import Hero from "@/components/shared/landing/hero";
 import UseCase from "@/components/shared/landing/use-case";
@@ -6,19 +8,17 @@ import Actions from "@/components/shared/landing/action";
 import CTA from "@/components/shared/landing/cta";
 import Offer from "@/components/shared/landing/offer";
 import Footer from "@/components/shared/landing/footer";
-import MaxContainer from "@/components/shared/common/maxcontainer";
 
 const Home = () => {
     return (
         <div className="bg-[#000000] w-[100vw] overflow-x-hidden">
             <MaxContainer
-                className="sm:bg-[url(/common/coins.png),url(/common/globe_flare.png)] bg-no-repeat sm:bg-[position:top_center,center_center] bg-[url(/common/coins_sm.png),url(/common/flare_mobile.png)] sm:bg-[length:100%_auto] bg-[length:150%_auto] bg-[position:center_top]"
-                
+                className="bg-[url(/common/mojuto-hero-bg.png)] max-w-[1440px] mx-auto h-[988px] bg-size-[length:100%_100%] bg-no-repeat bg-center relative"
             >
                 <Nav />
                 <Hero />
-               <UseCase />
             </MaxContainer>
+            <UseCase />
             <Features />
             <Offer />
             <Actions />
